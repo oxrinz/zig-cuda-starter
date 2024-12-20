aac535f26b2bbe02de40f6fb7132a3c90924fd93
rinz <roberts.denisovs@gmail.com>
2024-12-20 10:34:24 +0200

    better structure

--- example.py
+++ example.py
@@ -2,8 +2,8 @@ import ctypes
 
 lib = ctypes.CDLL('./zig-out/libstarter.so')  
 
-lib.addNumbers.argtypes = [ctypes.c_int, ctypes.c_int]
-lib.addNumbers.restype = ctypes.c_int
+lib.add.argtypes = [ctypes.c_int, ctypes.c_int]
+lib.add.restype = ctypes.c_int
 
 result = lib.add(5, 3)
 print(f"5 + 3 = {result}")
--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,31 @@
-const std = @import("std");
+extern fn cudaMalloc(ptr: *?*anyopaque, size: usize) c_int;
+extern fn cudaMemcpy(dest: *anyopaque, src: *const anyopaque, size: usize, kind: c_int) c_int;
+extern fn cudaFree(ptr: *anyopaque) c_int;
+extern "C" fn launchAddKernel(a: *anyopaque, b: *anyopaque, c: *anyopaque) void;
 
-pub extern "c" fn addNumbers(a: i32, b: i32) i32;
+const cudaMemcpyHostToDevice = 1;
+const cudaMemcpyDeviceToHost = 2;
 
 export fn add(a: i32, b: i32) i32 {
-    return addNumbers(a, b);
+    var d_a: ?*anyopaque = undefined;
+    var d_b: ?*anyopaque = undefined;
+    var d_c: ?*anyopaque = undefined;
+    var result: i32 = undefined;
+
+    _ = cudaMalloc(&d_a, @sizeOf(i32));
+    _ = cudaMalloc(&d_b, @sizeOf(i32));
+    _ = cudaMalloc(&d_c, @sizeOf(i32));
+
+    _ = cudaMemcpy(d_a.?, &a, @sizeOf(i32), cudaMemcpyHostToDevice);
+    _ = cudaMemcpy(d_b.?, &b, @sizeOf(i32), cudaMemcpyHostToDevice);
+
+    launchAddKernel(d_a.?, d_b.?, d_c.?);
+
+    _ = cudaMemcpy(&result, d_c.?, @sizeOf(i32), cudaMemcpyDeviceToHost);
+
+    _ = cudaFree(d_a.?);
+    _ = cudaFree(d_b.?);
+    _ = cudaFree(d_c.?);
+
+    return result;
 }
